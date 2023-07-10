069566430e69751659050dee09e46ccdc0df0e64
Renz Lacerna <renzlacerna13@gmail.com>
2023-07-10 19:24:56 -0400

    Updated Project 1

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Project-1-Sports
-To provide teams, game time, date, and statistics for Major League Baseball games using HTML, CSS, jQuery, and Javascript
+To provide NBAplayer's information such as team, conference, height, position, and weight using HTML, CSS, jQuery, and Javascript
+
 
 ![Project 1](images/wireframe.jpg)
 
-## Providing teams
-Being able to provide a team and who they play against with using a search bar. Allowing to show teams record of wins and losses to provide more details about the teams.
+## Providing Details
+Using a search engine, NBA players are able to be looked up and given details 
+
+
 
--- js/app.js
+++ js/app.js
@@ -1,2 +1,53 @@
-const BASE_URL = "https://api.sportsdata.io/v3/mlb/scores/json/pushauthgamestate?key=d6ca57d9ccf8411ab1c850d0ec05149d"
-const API_KEY = "d6ca57d9ccf8411ab1c850d0ec05149d"
+const BASE_URL = "https://www.balldontlie.io/api/v1/players?search=";
+
+const $team = $('#team');
+const $conference = $('#conference');
+const $abbreviation = $('#abbreviation');
+const $height = $('#height');
+const $position = $('#position');
+const $weight = $('#weight');
+const $input = $('input[type="text"]');
+
+$(document).ready(function() {
+    $('form').on('submit', handleGetData);
+});
+
+function handleGetData(event) {
+    event.preventDefault();
+    const userInput = $input.val().trim();
+
+if (userInput !== '') {
+$.ajax({
+    url: BASE_URL + userInput
+    }).then(
+        (data) => {
+            if (data.data.length > 0) {
+                const player = data.data[0]; 
+                render(player);
+            } else {
+                render(null);
+            }
+        },
+        (error) => {
+            console.log('Error:', error);
+            render(null);
+        }
+);
+}
+}
+
+function render(player) {
+    if (player !== null) {
+        $team.text('Team: ' + player.team.full_name);
+        $abbreviation.text('Abbreviation: ' + player.team.abbreviation);
+        $conference.text('Conference: ' + player.team.conference);
+        $height.text('Height: ' + player.height_feet + "ft" + player.height_inches + 'in');
+        $position.text('Position: ' + player.position);
+        $weight.text('Weight: ' + player.weight_pounds + 'lbs');
+    } else {
+        $team.text('');
+        $height.text('');
+        $position.text('');
+        $weight.text('Player not found');
+    }
+}
